21855a2fdf475c07406255007f168d0093afa157
dzikrisyairozi <dikiogres@gmail.com>
2023-10-29 14:06:14 +0700

    feat: program buidler boilerplate

---
+++ generator/program_builder.py
@@ -0,0 +1,10 @@
+class ProgramBuilder:
+    def __init__(self, grammar_structure):
+        self.grammar_structure = grammar_structure
+        self.template_file = "./templates/ray_template.py"
+    
+    def build_program(self):
+        # Placeholder for program building logic
+        generated_code = ""
+        # TODO: Implement the actual program building logic here, using the grammar_structure
+        return generated_code
