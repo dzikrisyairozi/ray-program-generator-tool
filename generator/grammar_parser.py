9f2c2d1df215fa854589780542285c79c07ed187
dzikrisyairozi <dikiogres@gmail.com>
2023-10-29 14:05:59 +0700

    feat: grammar parser boilerplate

---
+++ generator/grammar_parser.py
@@ -0,0 +1,9 @@
+class GrammarParser:
+    def __init__(self):
+        self.grammar_file = "./templates/predefined_grammar.txt"
+    
+    def parse_grammar(self):
+        # Placeholder for grammar parsing logic
+        grammar_structure = {}
+        # TODO: Implement the actual grammar parsing logic here
+        return grammar_structure
